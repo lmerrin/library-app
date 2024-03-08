b4b184d9309e3eb004463620af9367eb118817b4
Lilia <lilia@student.dev-island.org>
2024-03-07 20:29:28 -1000

    setup contact form struct and state

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,118 @@
 import { useState } from "react";
-import BookList from "./components/BookList/BookList";
-import SearchBar from "./components/SearchBar/SearchBar";
-import Header from "./components/Header/Header.jsx";
+import MaskedInput from "react-text-mask";
 import "./App.css";
 import dummyBooks from "./books.js";
+import BookList from "./Components/BookList/BookList.jsx";
+import SearchBar from "./Components/SearchBar/SearchBar.jsx";
 
 function App() {
   const [filteredBooks, setFilteredBooks] = useState(dummyBooks);
+  const [title, setTitle] = useState("");
+  const [firstName, setFirstName] = useState("");
+  const [lastName, setLastName] = useState("");
+  const [year, setYear] = useState("");
+  const [genre, setGenre] = useState("");
+  const [description, setDescription] = useState("");
+  // const [image, setImage] = useState("");
 
   return (
     <>
-      <Header  header/>
       <h1>Nā Puke o Hawaiʻi</h1>
       <SearchBar books={dummyBooks} setFilteredBooks={setFilteredBooks} />
+
+      <form>
+        <label>
+          Title:
+          <input
+            type="text"
+            required
+            className="form-input"
+            onChange={(event) => {
+              const value = event.target.value;
+              setTitle(value);
+            }}
+          />
+        </label>
+
+        <label>
+          Author First Name:
+          <input
+            type="text"
+            required
+            className="form-input"
+            onChange={(event) => {
+              const value = event.target.value;
+              setFirstName(value);
+            }}
+          />
+        </label>
+
+        <label>
+          Author Last Name:{" "}
+          <input
+            type="text"
+            required
+            className="form-input"
+            onChange={(event) => {
+              const value = event.target.value;
+              setLastName(value);
+            }}
+          />
+        </label>
+
+        <label>
+          Year:{" "}
+          <MaskedInput
+            mask={[/\d/, /\d/, /\d/, /\d/]}
+            guide={true}
+            required
+            pattern="\d{4}"
+            className="form-input"
+            placeholder="ex. 1997"
+            onChange={(event) => {
+              const value = event.target.value;
+              setYear(value);
+            }}
+          />
+        </label>
+
+        <label>
+          Genre:{" "}
+          <input
+            type="text"
+            required
+            className="form-input"
+            onChange={(event) => {
+              const value = event.target.value;
+              setGenre(value);
+            }}
+          />
+        </label>
+
+        <label>
+          Description:{" "}
+          <input
+            type="text"
+            required
+            className="form-input"
+            onChange={(event) => {
+              const value = event.target.value;
+              setDescription(value);
+            }}
+          />
+        </label>
+      </form>
+
+      <button
+        type="submit"
+        onClick={(event) => {
+          event.preventDefault();
+          console.log("Submit Clicked!");
+        }}
+      >
+        Create Reference
+      </button>
+
       <BookList books={filteredBooks} />
     </>
   );
